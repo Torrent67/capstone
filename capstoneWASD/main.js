39252f0dfd4f60cd38f57046459683c0ff77a842
Student <example@example.com>
2019-10-09 11:59:38 -0700

    working ish model

--- capstoneWASD/main.js
+++ capstoneWASD/main.js
@@ -2,141 +2,62 @@ var scene, camera, renderer, mesh;
 var meshFloor, ambientLight, light;
 
 var keyboard = {};
-var mouse = new THREE.Vector2();
-var player = { height:1.8, speed:1, turnSpeed:Math.PI*0.02 };
-var USE_WIREFRAME = true;
 var aspect = window.innerWidth / window.innerHeight;
+var player = { height:1.8, speed:1, turnSpeed:Math.PI*0.02 };
 var d = 100;
 
-
-//Loading screen
-var loadingScreen = {
-	scene: new THREE.Scene(),
-	camere: new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000),
-	box: new THREE.Mesh(
-		new THREE.BoxGeometry(0.5,0.5,0.5),
-		new THREE.MeshLambertMaterial({ colo:0x4444ff})
-	)
-};
-
-//Loading screen
-var LOADING_MANAGER = null;
-var RESOURSE_LOADED = false;
-
+//RENDERER
+renderer = new THREE.WebGLRenderer({
+	canvas: myCanvas,
+	antialias: true
+})
+renderer.setClearColor("#51565a")
+renderer.setSize(window.innerWidth, window.innerHeight);
 function init(){
 
-loadingScreen.box.position.set(0,0,5);
-loadingScreen.camere.lookAt(loadingScreen.box.position);
-loadingScreen.scene.add(loadingScreen.box);
+//CAMERA
+//SCENE
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+        camera.rotation.y = 45/180*Math.PI;
+        camera.position.x = 800;
+        camera.position.y = 100;
+        camera.position.z = 1000;
+	controls = new THREE.OrbitControls(camera);
+	controls.addEventListener('change', renderer);
 
-loadingManager = new THREE.LoadingManager();
-
-	scene = new THREE.Scene();
-	camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 1000 );
-	
-	mesh = new THREE.Mesh(
-		new THREE.TorusGeometry(30, 2, 20, 100 ),
-		new THREE.MeshBasicMaterial({color:0xff4444, wireframe:true})
-	);
-	mesh.position.y += 1;
-	// The cube can have shadows cast onto it, and it can cast shadows
-	mesh.receiveShadow = true;
-	mesh.castShadow = true;
-	scene.add(mesh);
-	
-	meshFloor = new THREE.Mesh(
-		new THREE.PlaneGeometry(300,300, 300,300),
-		// MeshBasicMaterial does not react to lighting, so we replace with MeshPhongMaterial
-		new THREE.MeshLambertMaterial({color:0x238dd3, wireframe:USE_WIREFRAME})
-		// See threejs.org/examples/ for other material types
-	);
-	meshFloor.rotation.x -= Math.PI / 2;
-	// Floor can have shadows cast onto it
-	meshFloor.receiveShadow = true;
-	scene.add(meshFloor);
-	
-
-	var loader = new THREE.GLTFLoader();
-	loader.load('rail_basic2_v1.glb', handle_load);
-
-	var mesh;
-
-	fuction handle_load(gltf){
-		mesh = gltf.scene.children[0];
-		scene.add(mesh);
-		mesh.position.z = -10;
-	}
-	
+//LIGHTS
+hlight = new THREE.AmbientLight (0x404040,10);
+scene.add(hlight);
+directionalLight = new THREE.DirectionalLight(0xffffff,1);
+directionalLight.position.set(0,1,0);
+directionalLight.castShadow = false;
+scene.add(directionalLight);
 
 
-	// LIGHTS
-	ambientLight = new THREE.AmbientLight(0xffffff, 1);
-	scene.add(ambientLight);
-	
-	light = new THREE.PointLight(0xff4444, 0.8, 0.8);
-	light.position.set(-3,6,-3);
-	light.castShadow = true;
-	// Will not light anything closer than 0.1 units or further than 25 units
-	light.shadow.camera.near = 0.1;
-	light.shadow.camera.far = 50;
-	scene.add(light);
-	
-	
-	camera.position.set( 80, 80, 80 );
-	camera.rotation.order = 'YXZ';
-	camera.rotation.y = - Math.PI / 4;
-	camera.rotation.x = Math.atan( - 2 / Math.sqrt( 4) );
-	
-	renderer = new THREE.WebGLRenderer();
-	renderer.setClearColor("#51565a")
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	
-	// Enable Shadows in the Renderer
-	renderer.shadowMap.enabled = true;
-	renderer.shadowMap.type = THREE.BasicShadowMap;
-	
-	document.body.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
-	// Model/material loading!
-	// var mtlLoader = new THREE.MTLLoader(loadingManager);
-	// mtlLoader.load("models/Brown_Waterfall_01.mtl", function(materials){
-		
-	// 	materials.preload();
-	// 	var objLoader = new THREE.OBJLoader(loadingManager);
-	// 	objLoader.setMaterials(materials);
-		
-	// 	objLoader.load("models/Brown_Waterfall_01.obj", function(mesh){
-		
-	// 		mesh.traverse(function(node){
-	// 			if( node instanceof THREE.Mesh ){
-	// 				node.castShadow = false;
-	// 				node.receiveShadow = false;
-	// 			}
-	// 		});
-		
-	// 		scene.add(mesh); 
-	// 		mesh.position.set(-4, 0, 4);
-	// 		mesh.rotation.y = -Math.PI/4;
-	// 	});
-		
-	// });
-	
+var loader = new THREE.GLTFLoader();
+loader.load('models/rail_basic2_v1.glb', function(gltf){
 
+	map = gltf.scene.children[0];
+	map.scale.set(0.5,0.5,0.5);
 
-    animate();
+	scene.add(gltf.scene);
+	animate();
+  });
 }
+function animate() {
+  renderer.render(scene,camera);
+  requestAnimationFrame(animate);
+}
+
+
 
 function animate() {
 
-	//loadscreen work in progress set to false to see a cube
-if( RESOURSE_LOADED == true){
-	requestAnimationFrame(animate);
-	loadingScreen.box.rotation.y -= 0.04;
-	loadingScreen.box.rotation.x += 0.04;
-	
-	renderer.render(loadingScreen.scene, loadingScreen.camere);
-	return;
-}
+
+
 
 
 
@@ -145,8 +66,7 @@ if( RESOURSE_LOADED == true){
 
     requestAnimationFrame(animate);
 
-    mesh.rotation.y += 0.01;
-    mesh.rotation.z += 2;
+    
 
     	// Keyboard movement inputs
 	if(keyboard[68]){ // D key
@@ -179,22 +99,14 @@ if( RESOURSE_LOADED == true){
 
 
 function keyDown(event) {
-    keyboard[event.keyCode] = true;
-}
-
-function keyUp(event) {
     keyboard[event.keyCode] = false;
 }
 
-function onMouseMove(event) {
-	event.preventDefault();
-	loadingScreen.box.rotation.y -= 0.04;
-	loadingScreen.box.rotation.x += 0.04;
-	mouse.x = RESOURCES_LOADED = true;
-	return;
+function keyUp(event) {
+    keyboard[event.keyCode] = true;
 }
 
-window.addEventListener('click', onMouseMove);
+
 window.addEventListener('keydown', keyDown);
 window.addEventListener('keyup', keyUp);
 
